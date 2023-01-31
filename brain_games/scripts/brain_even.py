499ee6575f2df6851efbc60f5d0cfeaca3834145
Igor <GIV_84@mail.ru>
2023-01-31 14:03:09 +0400

    исправил импорт brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
+from brain_games.games.even import brain_even
 from brain_games.games.brain_logics import brain_logics
-from brain_games.games.calc import brain_even
 
 
 def main():
