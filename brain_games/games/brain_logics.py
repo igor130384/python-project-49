71de00531f32b006737697ad6fd80ba9857d4826
Igor <GIV_84@mail.ru>
2023-01-31 15:53:42 +0400

    исправил строку в brain_logics

--- brain_games/games/brain_logics.py
+++ brain_games/games/brain_logics.py
@@ -5,11 +5,13 @@ def brain_logics(brain_logic):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
+    i = 0
     while i < 3:
         result = brain_logic()
         answer = prompt.string('Your answer: ')
         if int(answer) == int(result):
             print('Correct!')
+            i += 1
         else:
             print(
                 f"'{answer}' is wrong answer ;(. Correct "
@@ -17,6 +19,5 @@ def brain_logics(brain_logic):
             )
             break
 
-        i += 1
-    if i == 3:
-        print(f'Congratulations, {name}!')
+        if i == 3:
+            print(f'Congratulations, {name}!')
