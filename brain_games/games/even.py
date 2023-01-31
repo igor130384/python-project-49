108af714c71fa8545b6b3f656feb5dae0dfba80a
Igor <GIV_84@mail.ru>
2023-01-31 14:57:02 +0400

    исправил строку с ответом в even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,8 +2,8 @@ import random
 
 
 def brain_even():
-    task = 'Answer "yes" if the number is even, otherwise answer "no".'
     num = random.randint(1, 100)
+    task = 'Answer "yes" if the number is even, otherwise answer "no".'
     question = f'Question: {num}'
 
     def is_even(num):
