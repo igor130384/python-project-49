226d50767a1b7bd676442345c3b2cdce2a99949f
Igor <GIV_84@mail.ru>
2023-02-01 12:26:37 +0400

    add

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,16 +3,18 @@ import random
 
 def brain_progression():
     print('What number is missing in the progression?')
-    num1 = random.randint(1, 10)
-    num2 = random.randint(80, 100)
-    n = random.randint(2, 10)
-    numbers = []
-    for r in range(num1, num2, n):
-        numbers.append(r)
-        numbers.sort()
-    result = numbers[random.randint(1, 10)]
-    numbers.insert(result, "..")
-    string = " ".join(map(str, numbers[0:10]))
-    string = string.replace(str(result), '..')
-    print(f'Question: {string}')
+    list = []
+    step = random.randint(1, 10)
+    num1 = random.randint(0, 100)
+    while len(list) < 10:
+        list.append(num1)
+        num1 += step
+    index = random.randint(0, 9)
+    result = list[index]
+    b = ".."
+    list[index] = b
+    a = ''
+    for i in list:
+        a += f'{i} '
+    print('Question:', a)
     return result
