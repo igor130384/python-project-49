6d365ef3d0eefb37ea5b09f492baaa38559a41b1
Igor <GIV_84@mail.ru>
2023-02-01 09:12:16 +0400

    add

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,19 +1,22 @@
-import prompt
+
 
 import random
 
 
 def brain_calc():
+    print('What is the result of the expression?')
     operand = ['+', '-', '*']
     oper = random.choice(operand)
     num1 = random.randint(1, 100)
     num2 = random.randint(1, 100)
-    question = f'Question: {num1} {oper} {num2}'
-    answer = prompt.string('Your answer: ')
-    if oper == '+':
-        result = num1 + num2
-    elif oper == '-':
-        result = num1 - num2
-    else:
-        result = num1 * num2
-        return result, answer, question
+    print(f'Question: {num1} {oper} {num2}')
+
+    def is_calc(oper, num1, num2):
+        if oper == '+':
+            return num1 + num2
+        elif oper == '-':
+            return num1 - num2
+        else:
+            return num1 * num2
+    result = is_calc(oper, num1, num2)
+    return result
