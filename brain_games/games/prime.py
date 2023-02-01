a6fa1447f166ef38e5ea65db25524ca11c8efc7a
Igor <GIV_84@mail.ru>
2023-02-01 12:49:07 +0400

    add

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,12 +4,12 @@ import random
 def brain_prime():
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     num1 = random.randint(1, 100)
-    print('Question:', num1)
+
     for i in range(2, int(num1/2)+1):
         if (num1 % i) == 0:
             val = 'no'
             break
         else:
             val = 'yes'
-
+    print('Question:', num1)
     return val
