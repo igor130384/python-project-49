41ae3ee00a63accdac478ff8ba4731533d30eb53
Igor <GIV_84@mail.ru>
2023-02-08 13:04:58 +0400

    add

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,18 +1,20 @@
 import prompt
 
+NUMBER_OF_ROUNDS = 3
+
 
 def brain_logics(brain_logic):
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
     print(brain_logic.DESCRIPTION)
     count = 0
-    for _ in range(3):
+    for _ in range(NUMBER_OF_ROUNDS):
         question, result = brain_logic.generate_round()
         print(f"Question: {question}")
-        answer = prompt.string('Your answer: ')
+        answer = prompt.string("Your answer: ")
         if answer == str(result):
-            print('Correct!')
+            print("Correct!")
             count += 1
         else:
             print(
@@ -20,5 +22,5 @@ def brain_logics(brain_logic):
                 f"answer was '{result}'\nLet's try again, {name}!"
             )
             break
-    if count == 3:
-        print(f'Congratulations, {name}!')
+    if count == NUMBER_OF_ROUNDS:
+        print(f"Congratulations, {name}!")
